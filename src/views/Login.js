0e83d4f3f9cd1dc0e37b10e329497f1b51d799be
Ramon Ivan De Rama <74236420+ivanderama12@users.noreply.github.com>
2021-10-08 13:56:49 +0800

    Update Login.js

--- src/views/Login.js
+++ src/views/Login.js
@@ -51,7 +51,6 @@ const Login = () => {
     return (
         <div>
             <Navbar />
-            <h1>asdasd</h1>
             <SearchBar />
             <Container className='mt-3'>
                 <Row>
